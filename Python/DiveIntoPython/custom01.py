d72df5c6c0052f437e24cd3e37d643a46ddfbe3c
Yuriy Lyeshchenko <yuriylsh@live.com>
2014-08-12 17:20:04 -0500

    continue with the first chapter of Dive Into Python book

---
+++ Python/DiveIntoPython/custom01.py
@@ -0,0 +1,8 @@
+from ch01_first import approximate_size
+# alternative to the lines above:
+#		import ch01_first
+#		print(dir(ch01_first))
+print(approximate_size.__doc__)
+import sys
+print(sys.path)
+sys.path.insert(0, 'some_dirctory_path_to_look_for_py_files')
