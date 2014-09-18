7a188a5df21284f4b734c16eafd1dab4cbee7d10
Yuriy Lyeshchenko <yuriylsh@live.com>
2014-09-17 22:40:34 -0500

    DiveIntoPython: started working on unit testin

--- Python/DiveIntoPython/ch06_plural_file_of_patterns.py
+++ Python/DiveIntoPython/ch06_plural_file_of_patterns.py
@@ -11,7 +11,8 @@ def build_match_and_apply_functions(pattern, search, replace):
     return (matches_rule, apply_rule)
 
 rules = []
-with open('ch06_plural_file_of_patterns.txt', encoding='utf-8') as pattern_file:
+with open('ch06_plural_file_of_patterns.txt',
+          encoding='utf-8') as pattern_file:
     for line in pattern_file:
         pattern, search, replace = line.split(None, 3)
         rules.append(build_match_and_apply_functions(pattern, search, replace))
