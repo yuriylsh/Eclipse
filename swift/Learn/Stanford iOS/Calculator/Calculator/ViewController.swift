610e69b326720801e3b72c67fea791423fdca986
Yuriy Lyeshchenko <yuriylsh@live.com>
2015-08-24 23:39:20 -0500

    Moved Stanford iOS course from standalone location to bitbucket

---
+++ swift/Learn/Stanford iOS/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by imodules on 8/22/15.
+//  Copyright (c) 2015 imodules. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        
+    }
+}
+
