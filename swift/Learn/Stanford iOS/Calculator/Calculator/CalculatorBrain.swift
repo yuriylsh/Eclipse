ed57610f49e7825c9d017d93f8d46ba0c12abf9c
Yuriy Lyeshchenko <yuriylsh@live.com>
2015-08-26 22:53:53 -0500

    Swift stanford: at chapter 3

---
+++ swift/Learn/Stanford iOS/Calculator/Calculator/CalculatorBrain.swift
@@ -0,0 +1,46 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by imodules on 8/26/15.
+//  Copyright (c) 2015 imodules. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain{
+    private enum Op{
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+    }
+    
+    private var opStack = [Op]()
+    private var knownOperations = [String:Op]()
+    
+    init(){
+        knownOperations["✕"] = Op.BinaryOperation("✕", *)
+        knownOperations["÷"] = Op.BinaryOperation("÷") {$1 / $0}
+        knownOperations["+"] = Op.BinaryOperation("+", +)
+        knownOperations["-"] = Op.BinaryOperation("-") {$1 - $0}
+        knownOperations["√"] = Op.UnaryOperation("√", sqrt)
+    }
+    
+    func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Ops]){
+        
+    }
+    
+    func evaluate () -> Double?{
+        
+    }
+    
+    func pushOperand(operand: Double){
+        opStack.append(Op.Operand(operand))
+    }
+    
+    func performOperand(symbol: String){
+        if let operation = knownOperations[symbol]{
+            opStack.append(operation)
+        }
+    }
+}
