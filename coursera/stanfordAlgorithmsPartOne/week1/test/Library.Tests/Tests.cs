229c08fc4c0ffbcf8f64a8d7bb3a211284dc5fc0
Yuriy Lyeshchenko <yuriylsh@live.com>
2016-07-16 14:00:56 -0500

    renaming

--- coursera/stanfordAlgorithmsPartOne/week1/test/Library.Tests/Tests.cs
+++ coursera/stanfordAlgorithmsPartOne/week1/test/Library.Tests/Tests.cs
@@ -3,12 +3,32 @@ using Xunit;
 
 namespace Tests
 {
+    public class AssertionData
+    {
+        public AssertionData(string filePath, int result)
+        {
+            FilePath = filePath;
+            Result = result;
+        }
+
+        public string FilePath { get; }
+        public int Result { get; }
+        public static implicit operator AssertionData(string str)
+        {
+            var parts = str.Split(',');
+            return new AssertionData(parts[0], int.Parse(parts[1]));
+        }
+    }
+
     public class InversionsTest
     {
-        [Fact]
-        public void Test1() 
+        [Theory]
+        [InlineData("path1,5")]
+        public void Test1(string assertionDataString) 
         {
-            Assert.True(false);
+            AssertionData assertionData = assertionDataString;
+            var counter = new InversionsCounter();
+            Assert.Equal(assertionData.FilePath.Length, assertionData.Result);
         }
     }
 }
