8944f85f5f84e27aefe44c8090fa5c0024d5a432
Yuriy Lyeshchenko <yuriylsh@live.com>
2017-04-30 11:26:30 -0500

    some work on array rotations

--- interviews/HackerRank/Solutions/Cracking the Coding Interview/Arrays_LeftRotation.cs
+++ interviews/HackerRank/Solutions/Cracking the Coding Interview/Arrays_LeftRotation.cs
@@ -3,29 +3,22 @@
     //https://www.hackerrank.com/challenges/ctci-array-left-rotation
     public static class Arrays_LeftRotation
     {
-        public static void Rotate(string[] array, int rotations)
+        public static string[] Rotate(string[] array, int rotations)
         {
             var length = array.Length;
-            var swapPosition = 0;
-            do
+            int rotationOffset = length - rotations;
+            var result = new string[length];
+            for (int i = 0; i < length; i++)
             {
-                var swapWith = GetSwapPosition(length - rotations + swapPosition, length);
-                SwapArrayElements(array, 0, swapWith);
-                swapPosition = swapWith;
-            } while (swapPosition != 0);
+                result[GetSwapPosition(rotationOffset + i, length)] = array[i];
+            }
+            return result;
         }
 
         private static int GetSwapPosition(int swapWith, int length)
         {
             var adjusted = swapWith - length;
             return adjusted < 0 ? swapWith : adjusted;
-        }
-
-        private static void SwapArrayElements(string[] array, int a, int b)
-        {
-            string temp = array[a];
-            array[a] = array[b];
-            array[b] = temp;
         }
     }
 }
--- interviews/HackerRank/Tests/Cracking the Coding Interview/Arrays_LeftRotationTests.cs
+++ interviews/HackerRank/Tests/Cracking the Coding Interview/Arrays_LeftRotationTests.cs
@@ -16,30 +16,42 @@ namespace Tests
         {
             string[] input = { "1", "2", "3", "4", "5" };
 
-            Arrays_LeftRotation.Rotate(input, swaps);
+            var result = Arrays_LeftRotation.Rotate(input, swaps);
 
-            input.Should().Equal(expected);
+            result.Should().Equal(expected);
+        }
+
+        [Theory]
+        [InlineData(3, new[] { "4", "1", "2", "3" })]
+        [InlineData(2, new[] { "3", "4", "1", "2" })]
+        [InlineData(1, new[] { "2", "3", "4", "1" })]
+        public void Rotate_EvenElementsCountArrayDoesSampleRotations_DoesExpectedRotations(int swaps, string[] expected)
+        {
+            string[] input = { "1", "2", "3", "4" };
+
+            var result = Arrays_LeftRotation.Rotate(input, swaps);
+
+            result.Should().Equal(expected);
         }
 
         [Fact]
         public void Rotate_SingleElementArrayDoesOneRotation_DoesExpectedRotation()
         {
-            var input = new[] {"33"};
+            var input = new[] { "33" };
 
-            Arrays_LeftRotation.Rotate(input, 1);
+            var result = Arrays_LeftRotation.Rotate(input, 1);
 
-            input.Should().Equal("33");
+            result.Should().Equal("33");
         }
 
         [Fact]
         public void Rotate_TestCase1_DoesExpectedRotations()
         {
-            var input = new[] { "41","73","89","7","10","1","59","58","84","77","77","97","58","1","86","58","26","10","86","51" };
+            var input = new[] { "41", "73", "89", "7", "10", "1", "59", "58", "84", "77", "77", "97", "58", "1", "86", "58", "26", "10", "86", "51" };
 
-            Arrays_LeftRotation.Rotate(input, 10);
+            var result = Arrays_LeftRotation.Rotate(input, 10);
 
-            input.Should().Equal("77","97","58","1","86","58","26","10","86","51","41","73","89","7","10","1","59","58","84","77");
+            result.Should().Equal("77", "97", "58", "1", "86", "58", "26", "10", "86", "51", "41", "73", "89", "7", "10", "1", "59", "58", "84", "77");
         }
-
     }
 }
