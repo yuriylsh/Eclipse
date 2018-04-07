c36e8b098d2efeb553a21e0b02ae7338818cf1aa
Yuriy Lyeshchenko <yuriylsh@live.com>
2018-04-07 17:45:16 -0500

    started ex14

--- Unity/Coursera/IntroductionProgrammingUnity/Exercise13/Assets/scripts/Person.cs
+++ Unity/Coursera/IntroductionProgrammingUnity/Exercise13/Assets/scripts/Person.cs
@@ -11,7 +11,7 @@ public class Person : MonoBehaviour {
         {
             var angle = Random.Range(0, 2 * Mathf.PI);
             var direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-            var magnitude = Random.Range(1f, 5f);
+            var magnitude = Random.Range(5f, 15f);
             return direction * magnitude;
         }
     }
---
+++ Unity/Coursera/IntroductionProgrammingUnity/Exercise14/Exercise14/Assets/scripts/Jumper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Jumper : MonoBehaviour 
+{
+    
+	
+	void Update () {
+	    var h = Screen.height;
+	}
+}
