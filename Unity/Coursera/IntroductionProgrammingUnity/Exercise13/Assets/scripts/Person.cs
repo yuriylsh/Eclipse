f82b226982b740ee65d07c2fa39f900c4405307c
Yuriy Lyeshchenko <yuriylsh@live.com>
2018-04-07 15:44:39 -0500

    continue working on Ex13

---
+++ Unity/Coursera/IntroductionProgrammingUnity/Exercise13/Assets/scripts/Person.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Person : MonoBehaviour {
+	void Start () => ApplyRandomForce();
+
+    private void ApplyRandomForce() => GetComponent<Rigidbody2D>().AddForce(RandomForce, ForceMode2D.Impulse);
+
+    private Vector2 RandomForce
+    {
+        get
+        {
+            var angle = Random.Range(0, 2 * Mathf.PI);
+            var direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+            var magnitude = Random.Range(1f, 5f);
+            return direction * magnitude;
+        }
+    }
+}
