c1f8659f13be4b28c1d19fa62baad9e1cb07ab68
Yuriy Lyeshchenko <yuriylsh@live.com>
2018-11-08 06:53:14 -0600

    removing redux and routing

--- interviews/adPerspective/Yuriy.Web/ClientApp/src/index.tsx
+++ interviews/adPerspective/Yuriy.Web/ClientApp/src/index.tsx
@@ -1,29 +1,7 @@
-// tslint:disable-next-line:ordered-imports
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
-// tslint:disable-next-line:ordered-imports
-import { createBrowserHistory } from 'history';
-import configureStore from './store/configureStore';
 import { Root } from './components/Root';
 import './index.scss'
 
-
-// Create browser history to use in the Redux store
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') as string;
-const history = createBrowserHistory({ basename: baseUrl });
-
-// Get the application-wide store instance, prepopulating with state from the server where available.
-const initialState = (window as any).initialReduxState;
-const store = configureStore(history, initialState);
-
 const rootElement = document.getElementById('root');
-
-ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Root />
-    </ConnectedRouter>
-  </Provider>,
-  rootElement);
+ReactDOM.render(<Root />, rootElement);
