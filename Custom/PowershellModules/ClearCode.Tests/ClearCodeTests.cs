40087d6e8258ce037175b30218154338d847038b
Yuriy Lyeshchenko <yuriy@adpredictive.com>
2018-12-30 14:27:00 -0600

    Implementing GetProjectFile

--- Custom/PowershellModules/ClearCode.Tests/ClearCodeTests.cs
+++ Custom/PowershellModules/ClearCode.Tests/ClearCodeTests.cs
@@ -6,19 +6,45 @@ namespace ClearCode.Tests
 {
     public class ClearCodeTests
     {
-        DirectoryInfo _projectDirectory;
+        private readonly string _projectDirectory;
+        private string _errorMessage;
+        private readonly ProjectUtils _subject;
 
         public ClearCodeTests()
         {
-            _projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent;
+            _projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            _subject = new ProjectUtils(error => _errorMessage = error);
         }
 
         [Fact]
-        public void FindProjectFiles_OneProject_ReturnsPathToProjectFile()
+        public void FindProjectFiles_OneProjectAtRoot_ReturnsPathToProjectFile()
         {
-            //var subject = new ProjectUtils(_ => {});
-            //subject.GetProjectFile(_projectDirectory).Should().Be(Path.Combine(_projectDirectory, "ClearCode.Tests.csproj"))
+            var result = _subject.GetProjectFile(_projectDirectory);
 
+            result.Should().Be(Path.Combine(_projectDirectory, "ClearCode.Tests.csproj"));
+            _errorMessage.Should().BeNull();
+        }
+
+        [Fact]
+        public void FindProjectFiles_MultipleProjectsAtRoot_LogsErrorReturnsNull()
+        {
+            var root = Path.Combine(_projectDirectory, "MultipleProjFiles");
+            var expectedErrorMessage =
+                $"Expected only one .csproj file in the current directory. Found 2 project files instead: '{Path.Combine(root, "project1.csproj")}', '{Path.Combine(root, "project2.csproj")}'";
+            
+            var result = _subject.GetProjectFile(root);
+
+            result.Should().BeNull();
+            _errorMessage.Should().Be(expectedErrorMessage);
+        }
+
+        [Fact]
+        public void FindProjectFiles_NoProjectsAtRoot_LogsErrorReturnsNull()
+        {
+            var result = _subject.GetProjectFile(Path.Combine(_projectDirectory, "NoProjectFiles"));
+
+            result.Should().BeNull();
+            _errorMessage.Should().Be("Expected one .csproj file in the current directory. Found none.");
         }
     }
 }
--- Custom/PowershellModules/ClearCode/ProjectUtils.cs
+++ Custom/PowershellModules/ClearCode/ProjectUtils.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace ClearCode
 {
@@ -9,6 +11,27 @@ namespace ClearCode
         public ProjectUtils(Action<string> logError)
         {
             _logError = logError;
+        }
+
+        public string GetProjectFile(string directory)
+        {
+            var foundProjects = Directory.GetFiles(directory, "*.csproj", SearchOption.TopDirectoryOnly);
+
+            switch (foundProjects.Length)
+            {
+                case 1:
+                    return foundProjects[0];
+                case 0:
+                    _logError("Expected one .csproj file in the current directory. Found none.");
+                    break;
+                default:
+                    var multipleFiles = string.Join(", ", foundProjects.Select(x => "'" + x + "'"));
+                    _logError(
+                        $"Expected only one .csproj file in the current directory. Found {foundProjects.Length} project files instead: {multipleFiles}");
+                    break;
+            }
+            
+            return null;
         }
     }
 }
