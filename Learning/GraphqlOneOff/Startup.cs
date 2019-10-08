05ce42f87031e381e43dcaffc7e5a06501e81252
Yuriy Lyeshchenko <yuriylsh@live.com>
2019-10-07 22:30:34 -0500

    fix an issue with synchronous IO caused by using json.net in graphql

--- Learning/GraphqlOneOff/Startup.cs
+++ Learning/GraphqlOneOff/Startup.cs
@@ -6,10 +6,11 @@ using GraphQL;
 using GraphQL.Server;
 using GraphQL.Server.Ui.Playground;
 using GraphqlOneOff.DAL;
-using GraphqlOneOff.Queries;
+using GraphqlOneOff.GraphQl;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -17,37 +18,30 @@ namespace GraphqlOneOff
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<KestrelServerOptions>(AllowSynchronousIO); // https://github.com/graphql-dotnet/graphql-dotnet/issues/1116
+            
             services.AddScoped<IDependencyResolver>(sp => new FuncDependencyResolver(sp.GetRequiredService));
             services.AddGraphQL(x => { x.ExposeExceptions = true; }).AddGraphTypes();
-            services.AddScoped<CategoryQuery>();
             services.AddScoped<MainSchema>();
+            
             
             services.AddSingleton<GetAllCategories>(FakeDataProvider.GetAllCategories);
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseRouting();
-
-//            app.UseEndpoints(endpoints =>
-//            {
-//                endpoints.MapGet("/", async context =>
-//                {
-//                    await context.Response.WriteAsync("Hello World!");
-//                });
-//            });
+            
             app.UseGraphQL<MainSchema>(); // by default /graphql
             app.UseGraphQLPlayground(new GraphQLPlaygroundOptions{Path = "/"}); // by default /ui/playground
         }
+
+        private static void AllowSynchronousIO(KestrelServerOptions options) => options.AllowSynchronousIO = true;
     }
 }
