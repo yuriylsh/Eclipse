b7aac7ec220cf70bd925e48d05f214669e94ea7b
Yuriy Lyeshchenko <yuriylsh@live.com>
2019-12-26 14:40:39 -0600

    day 01 part 2

---
+++ Learning/adventofcode/Year2019/Solutions/Day01.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Solutions
+{
+    public static class Day01
+    {
+        public static int Part1(FileInfo input) => GetInputFromFile(input).Select(GetFuel).Sum();
+
+        public static int Part2(FileInfo input) => GetInputFromFile(input).Select(GetFuelRecursive).Sum();
+
+        public static int GetFuel(int mass) => mass / 3 - 2;
+
+        public static int GetFuelRecursive(int mass)
+        {
+            var fuelForMass = GetFuel(mass);
+            if (fuelForMass <= 0) return 0;
+            return fuelForMass +  GetFuelRecursive(fuelForMass);
+        }
+
+        private static IEnumerable<int> GetInputFromFile(FileInfo input)
+            => File.ReadLines(input.FullName).Select(int.Parse);
+    }
+}
--- Learning/adventofcode/Year2019/Tests/Day01Tests.cs
+++ Learning/adventofcode/Year2019/Tests/Day01Tests.cs
@@ -13,21 +13,40 @@ namespace Tests
         {
             var input = new FileInfo(Part1InputFilePath);
 
-            var result = Day1_Part1.GetFuelTotal(input);
+            var result = Day01.Part1(input);
 
             result.Should().Be(3299598);
         }
+        
+        [Fact]
+        public void Part2_Input_ReturnsCorrectFuelSum()
+        {
+            var input = new FileInfo(Part1InputFilePath);
+
+            var result = Day01.Part2(input);
+
+            result.Should().Be(4946546);
+        }
+        
+        [Theory]
+        [InlineData(12, 2)]
+        [InlineData(14, 2)]
+        [InlineData(1969, 966)]
+        [InlineData(100756, 50346)]
+        public void GetFuelRecursive_Examples_ReturnsCorrectFuel(int mass, int expectedFuel) =>
+            Day01.GetFuelRecursive(mass).Should().Be(expectedFuel);
+        
         
         [Theory]
         [InlineData(12, 2)]
         [InlineData(14, 2)]
         [InlineData(1969, 654)]
         [InlineData(100756, 33583)]
-        public void Part1_Examples_ReturnsCorrectFuel(int mass, int expectedFuel) =>
-            Day1_Part1.GetFuel(mass).Should().Be(expectedFuel);
+        public void GetFuel_Examples_ReturnsCorrectFuel(int mass, int expectedFuel) =>
+            Day01.GetFuel(mass).Should().Be(expectedFuel);
 
         [Fact]
-        public void Part1_Input_ShouldExist()
+        public void Input_ShouldExist()
         {
             var input = new FileInfo(Part1InputFilePath);
             
