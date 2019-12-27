c97ed1abb422364e81068be849091fdf6f9e7415
Yuriy Lyeshchenko <yuriylsh@live.com>
2019-12-27 09:33:08 -0600

    day04 part 2

--- Learning/adventofcode/Year2019/Solutions/Day04.cs
+++ Learning/adventofcode/Year2019/Solutions/Day04.cs
@@ -6,18 +6,55 @@ namespace Solutions
 {
     public static class Day04
     {
-        public static int Part1(int start, int end) => Enumerable.Range(start, end - start + 1).Count(MeetsCriteria);
+        public static int Part1(int start, int end) => Enumerable.Range(start, end - start + 1).Count(MeetsPart1Criteria);
+        
+        public static int Part2(int start, int end) => Enumerable.Range(start, end - start + 1).Count(MeetsPart2Criteria);
 
-        public static bool MeetsCriteria(int number)
+        public static bool MeetsPart1Criteria(int number)
         {
             var input = ToArray(number);
             var criteria = new Predicate<int[]>[] {HasTwoConsecutiveEqualDigits, NonDecreasing};
+            return MeetsCriteria(input, criteria);
+        }
+        
+        public static bool MeetsPart2Criteria(int number)
+        {
+            var input = ToArray(number);
+            var criteria = new Predicate<int[]>[] {HasOnlyTwoConsecutiveEqualDigits, NonDecreasing};
             return MeetsCriteria(input, criteria);
         }
 
         private static bool MeetsCriteria(int[] number, IEnumerable<Predicate<int[]>> predicates) 
             => predicates.All(x => x(number));
 
+        private static bool HasOnlyTwoConsecutiveEqualDigits(int[] number)
+        {
+            Span<int> lengths = stackalloc int[number.Length - 1];
+            for (var i = 0; i < lengths.Length; i++)
+            {
+                var currentDigit = number[i];
+                ref var target = ref lengths[i];
+                target = 1;
+                for (var j = i + 1; j < number.Length; j++)
+                {
+                    if (currentDigit == number[j])
+                    {
+                        target += 1;
+                        i += 1;
+                        continue;
+                    }
+                    break;
+                }
+            }
+
+            foreach (var length in lengths)
+            {
+                if (length == 2) return true;
+            }
+
+            return false;
+        }
+        
         private static bool HasTwoConsecutiveEqualDigits(int[] number)
         {
             for (var i = 0; i < number.Length - 1; i++)
--- Learning/adventofcode/Year2019/Tests/Day04Tests.cs
+++ Learning/adventofcode/Year2019/Tests/Day04Tests.cs
@@ -7,18 +7,28 @@ namespace Tests
     public class Day04Tests
     {
         [Fact]
-        public void Part1_GivenRange_ReturnsCorrectCount()
-        {
-            Day04.Part1(387638, 919123).Should().Be(2);
-        }
+        public void Part1_GivenRange_ReturnsCorrectCount() => Day04.Part1(387638, 919123).Should().Be(466);
         
         [Fact]
-        public void MeetsCriteria_111111_ReturnsTrue() => Day04.MeetsCriteria(111111).Should().BeTrue();
+        public void Part2_GivenRange_ReturnsCorrectCount() => Day04.Part2(387638, 919123).Should().Be(292);
 
         [Fact]
-        public void MeetsCriteria_223450_ReturnsFalseDecreases() => Day04.MeetsCriteria(223450).Should().BeFalse();
+        public void MeetsPart1Criteria_111111_ReturnsTrue() => Day04.MeetsPart1Criteria(111111).Should().BeTrue();
 
         [Fact]
-        public void MeetsCriteria_123789_ReturnsFalseNoDouble() => Day04.MeetsCriteria(123789).Should().BeFalse();
+        public void MeetsPart1Criteria_223450_ReturnsFalseDecreases() => Day04.MeetsPart1Criteria(223450).Should().BeFalse();
+
+        [Fact]
+        public void MeetsPart1Criteria_123789_ReturnsFalseNoDouble() => Day04.MeetsPart1Criteria(123789).Should().BeFalse();
+
+        [Theory]
+        [InlineData(112233, true)]
+        [InlineData(123444, false)]
+        [InlineData(111122, true)]
+        [InlineData(223450, false)]
+        [InlineData(123789, false)]
+        public void MettsPart2Criteria_ReturnsCorrectAnswer(int number, bool expectedResult) =>
+            Day04.MeetsPart2Criteria(number).Should().Be(expectedResult);
+
     }
 }
