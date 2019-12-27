336263d15651f5bbc7f81e9a3e8f05af982c9c0b
Yuriy Lyeshchenko <yuriylsh@live.com>
2019-12-27 00:05:09 -0600

    day04 part 1 WIP

---
+++ Learning/adventofcode/Year2019/Solutions/Day04.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Solutions
+{
+    public static class Day04
+    {
+        public static bool MeetsCirteria(int number)
+        {
+            var a = ToArray(number);
+            return true;
+        }
+
+        private static int[] ToArray(int x)
+        {
+            var length = (int) Math.Log10(x) + 1;
+            var result = new int[length];
+            for (var i = 1; i <= length; i++)
+            {
+                result[^i] = x % 10;
+                x /= 10;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Learning/adventofcode/Year2019/Tests/Day04Tests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Solutions;
+using Xunit;
+
+namespace Tests
+{
+    public class Day04Tests
+    {
+        [Fact]
+        public void MeetsCriteria_111111_ReturnsTrue()
+        {
+            Day04.MeetsCirteria(111111).Should().BeTrue();
+        }
+        
+        [Fact]
+        public void MeetsCriteria_223450_ReturnsFalse()
+        {
+            Day04.MeetsCirteria(223450).Should().BeFalse();
+        }
+    }
+}
