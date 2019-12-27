309f5af6d9b049a04d3040704534ef3e01845ab0
Yuriy Lyeshchenko <yuriylsh@live.com>
2019-12-27 08:53:18 -0600

    day04 part 1

--- Learning/adventofcode/Year2019/Solutions/Day04.cs
+++ Learning/adventofcode/Year2019/Solutions/Day04.cs
@@ -1,22 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Solutions
 {
     public static class Day04
     {
-        public static bool MeetsCirteria(int number)
+        public static int Part1(int start, int end) => Enumerable.Range(start, end - start + 1).Count(MeetsCriteria);
+
+        public static bool MeetsCriteria(int number)
         {
-            var a = ToArray(number);
-            return true;
+            var input = ToArray(number);
+            var criteria = new Predicate<int[]>[] {HasTwoConsecutiveEqualDigits, NonDecreasing};
+            return MeetsCriteria(input, criteria);
         }
 
+        private static bool MeetsCriteria(int[] number, IEnumerable<Predicate<int[]>> predicates) 
+            => predicates.All(x => x(number));
+
+        private static bool HasTwoConsecutiveEqualDigits(int[] number)
+        {
+            for (var i = 0; i < number.Length - 1; i++)
+            {
+                if (number[i] == number[i + 1]) return true;
+            }
+
+            return false;
+        }
+
+        private static bool NonDecreasing(int[] number)
+        {
+            for (int i = 0; i < number.Length - 1; i++)
+            {
+                if (number[i] > number[i + 1]) return false;
+            }
+
+            return true;
+        }
+            
         private static int[] ToArray(int x)
         {
             var length = (int) Math.Log10(x) + 1;
             var result = new int[length];
-            for (var i = 1; i <= length; i++)
+            for (var i = length - 1; i >=0; i--)
             {
-                result[^i] = x % 10;
+                result[i] = x % 10;
                 x /= 10;
             }
 
--- Learning/adventofcode/Year2019/Tests/Day04Tests.cs
+++ Learning/adventofcode/Year2019/Tests/Day04Tests.cs
@@ -7,15 +7,18 @@ namespace Tests
     public class Day04Tests
     {
         [Fact]
-        public void MeetsCriteria_111111_ReturnsTrue()
+        public void Part1_GivenRange_ReturnsCorrectCount()
         {
-            Day04.MeetsCirteria(111111).Should().BeTrue();
+            Day04.Part1(387638, 919123).Should().Be(2);
         }
         
         [Fact]
-        public void MeetsCriteria_223450_ReturnsFalse()
-        {
-            Day04.MeetsCirteria(223450).Should().BeFalse();
-        }
+        public void MeetsCriteria_111111_ReturnsTrue() => Day04.MeetsCriteria(111111).Should().BeTrue();
+
+        [Fact]
+        public void MeetsCriteria_223450_ReturnsFalseDecreases() => Day04.MeetsCriteria(223450).Should().BeFalse();
+
+        [Fact]
+        public void MeetsCriteria_123789_ReturnsFalseNoDouble() => Day04.MeetsCriteria(123789).Should().BeFalse();
     }
 }
