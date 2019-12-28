95fa4e69bed2d4dc7d9282f9bc1c14536fa1f58d
Yuriy Lyeshchenko <yuriylsh@live.com>
2019-12-28 14:39:50 -0600

    before fixing input

---
+++ Learning/adventofcode/Year2019/Solutions/Day05.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Solutions.Shared;
+
+namespace Solutions
+{
+    public class Day05
+    {
+        public static IReadOnlyCollection<int> Part1(FileInfo sourceCode)
+        {   
+            var input = new Queue<int>();
+            input.Enqueue(1);
+            var outputs = new List<int>();
+            Action<int> output = x => outputs.Add(x);
+            var program = IntcodeComputer.Parse(File.ReadAllText(sourceCode.FullName));
+            
+            IntcodeComputer.Run(program, input, output);
+
+            return outputs;
+        }
+    }
+}
---
+++ Learning/adventofcode/Year2019/Tests/Day05Tests.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using FluentAssertions;
+using Solutions;
+using Xunit;
+
+namespace Tests
+{
+    public class Day05Tests
+    {
+        [Fact]
+        public void Part1_InputProgram_ProducesCorrectDiagnosticsCode()
+        {
+            var result = Day05.Part1(new FileInfo("Inputs/day05_part1_input.txt"));
+
+            result.Should().Equal(0, 0, 0, 0, 0, 0, 0, 0, 0, 223);
+        }
+    }
+}
