988c0d386f6c3d0d57595aa5676aa4048615258d
Yuriy Lyeshchenko <yuriylyeshchenko@filevine.com>
2021-01-04 11:34:16 -0600

    Advent of Code 2020 day 13 part 2 - works with sample, too slow for real input

--- Learning/adventofcode/Year2020/Solutions/Day13.cs
+++ Learning/adventofcode/Year2020/Solutions/Day13.cs
@@ -45,6 +45,15 @@ namespace Solutions
             var timestamp = Day13Solution.GetTimestamp(schedule);
             timestamp.Should().Be(expectedTimestamp);
         }
+        
+        [Fact]
+        public void Part2_Input_CorrectTimestamp()
+        {
+            var schedule = "37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,601,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,17,x,x,x,x,x,23,x,x,x,x,x,29,x,443,x,x,x,x,x,x,x,x,x,x,x,x,13";
+
+            var timestamp = Day13Solution.GetTimestamp(schedule);
+            timestamp.Should().Be(1L);
+        }
     }
 
     public class Day13Solution
@@ -69,7 +78,46 @@ namespace Solutions
 
         public static long GetTimestamp(string schedule)
         {
-            throw new System.NotImplementedException();
+            var busses = GetBusses(schedule);
+            var max = GetMaxBus(busses);
+
+            var coef = 1L;
+            var timestamp = 0L;
+            while (true)
+            {
+                timestamp = GetTimestamp(max, coef);
+                if (busses.All(x => (timestamp + x.offset) % x.number == 0)) return timestamp;
+                coef += 1;
+            }
+
+            static long GetTimestamp((int number, int offset) bus, long coef) => bus.number * coef - bus.offset;
+
+            static (int number, int offset) GetMaxBus(List<(int number, int offset)> busses)
+            {
+                (int number, int offset) max = (-1, -1);
+                foreach (var item in busses)
+                {
+                    if (item.number > max.number) max = item;
+                }
+
+                return max;
+            }
+        }
+
+        private static List<(int number, int offset)> GetBusses(string schedule)
+        {
+            var split = schedule.Split(',');
+            List<(int number, int offset)> busses = new();
+            for (var i = 0; i < split.Length; i++)
+            {
+                var current = split[i];
+                if (current != "x")
+                {
+                    busses.Add((int.Parse(current), i));
+                }
+            }
+
+            return busses;
         }
     }
 }
